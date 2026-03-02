a9cb6e0425f09fbc2758e846d4e85e933538c9ae
EricNeed <EricNiB2019@gmail.com>
2026-03-01 22:20:32 -0600

    big big uppdate

--- interactive_scene/include/sprite.js
+++ interactive_scene/include/sprite.js
@@ -1,94 +1,217 @@
+//****************************************************objects
 class Sprite{  
   constructor(newX, newY, image_dir){
     this.x = newX;
     this.y = newY;
     this.image_object = loadImage(image_dir);
+    this.image_flip = false;
     this.hitbox_paddingX = 7;
     this.hitbox_paddingY = 6;
     this.walk_speed = 1;
+    this.is_climbing = false;
+    this.is_indoor = true;
+    this.using_block = false;
+    this.beside_functional_block = false;//will be a object to the using block
   }
 }
 
-class SpriteManager{
-  constructor(){
-    this.currentID = 0;
-    this.sprite_list = [];
-  }
-  
-  createSprite(x, y, image_dir){
-    this.sprite_list[this.currentID] = new Sprite(x, y, image_dir);
-    this.currentID += 1;
-    return this.currentID - 1;
-  }
-}
-
-//***************************************************collision
 class box{
   constructor(newX, newY, newDX, newDY){
     this.x = newX;
     this.y = newY;
     this.dx = newDX;
     this.dy = newDY;
-    this.openings = [];
+    this.openings = [];//use to place functional block for boxes, 0=is box, 2=special script
+    this.image_object;
+    this.attribute1;
   }
 }
 
+//***************************************************sprite_manager
+class SpriteManager{
+  constructor(){
+    this.sprite_list = [];
+  }
+  
+  createSprite(x, y, image_dir){
+    let current_index = this.sprite_list.length;
+    this.sprite_list[current_index] = new Sprite(x, y, image_dir);
+    return current_index;
+  }
+}
+
+//***************************************************barriers
 
 class BarrierManager{
   constructor(){
-    this.current_boxID = 0;
-    this.current_chamberID = 0;
     this.chamber = [];//player can go in but cant go out
     this.box = [];//player cannot go in, only collide with it
+    this.ladder = [];
+    this.terrain = [];//the stuff ship take damage from
   }
   addChamber(x, y, dx, dy){
-    this.chamber[this.current_chamberID] = new box(x, y, dx, dy);
-    this.current_chamberID += 1;
-    return this.current_chamberID -1;
+    let current_index = this.chamber.length;
+    this.chamber[current_index] = new box(x, y, dx, dy);
+    return current_index;
   }
   addBox(x, y, dx, dy){
-    this.box[this.current_boxID] = new box(x, y, dx, dy);
-    this.current_boxID += 1;
-    return this.current_boxID -1;
+    let current_index = this.box.length;
+    this.box[current_index] = new box(x, y, dx, dy);
+    return current_index;
+  }
+  addLadder(newX, newY){
+    let current_index = this.ladder.length;
+    this.ladder[current_index] = new box(newX, newY, 10, 18);
+    this.ladder[current_index].image_object = loadImage("assets/ladder.png");
+    return current_index;
+  }
+  addTerrain(x, y, dx, dy){
+    let current_index = this.terrain.length;
+    this.terrain[current_index] = new box(x, y, dx, dy);
+    return current_index;
   }
 }
 
+//script for what happens if player interact with those blocks
+class DriverSeat{
+  working(display_mult, ship_propertie){
+    fill(0);
+    textSize(7 * display.mult);
+    smooth();
+    text(`X Velocity: ${ship_propertie.VelocityX} \nY Velocity: ${ship_propertie.VelocityY}`, (display.DEFAULT_CANVA - 80) * display.mult, 40 * display.mult)
+    text("you are contoling the ship, press R to escape", 200 * display_mult, 380 * display_mult);
+
+    if(keyIsDown(87)){//w
+      ship_propertie.VelocityY -= 0.01;
+    }else if(keyIsDown(83)){//s
+      ship_propertie.VelocityY += 0.01;
+    }else if(ship_propertie.VelocityY !== 0){
+      ship_propertie.VelocityY -= Math.sign(ship_propertie.VelocityY) * 0.01;
+    }
+    if(keyIsDown(65)){//a
+      ship_propertie.VelocityX -= 0.01;
+    }else if(keyIsDown(68)){//d
+      ship_propertie.VelocityX += 0.01;
+    }else if(ship_propertie.VelocityX !== 0){
+      ship_propertie.VelocityX -= Math.sign(ship_propertie.VelocityX) * 0.01;
+    }
+
+    ship_propertie.shipX += ship_propertie.VelocityX;
+    ship_propertie.shipY += ship_propertie.VelocityY;
+  }
+}
+
+class GunnerSeat{
+  working(){
+
+  }
+}
+//***************************************************collision
 class Collision{
   constructor(s_m_temp, b_m_temp){
     this.s_m = s_m_temp;
     this.b_m = b_m_temp;
   }
-  tickCollision(){
+
+  //give the feed back of how much 1 is suppose to move from 2 if its in a chamber.
+  checkDist(x1, y1, endx1, endy1, x2, y2, endx2, endy2){
+    let x_dst = x2 - x1;//positive if out
+    let y_dst = y2 - y1;//positive if out
+    let dx_dst = endx2 - endx1;//negative if out
+    let dy_dst = endy2 - endy1;//negative if out
+    return [x_dst, y_dst, dx_dst, dy_dst];
+  }
+
+  tickCollision(playerID, ship_propertie, display){
     for(let s = 0; s < this.s_m.sprite_list.length; s++){
       let sprite = this.s_m.sprite_list[s];
+      let hitbox_dx = sprite.image_object.width - sprite.hitbox_paddingX*2;
+      let hitbox_dy = sprite.image_object.height - sprite.hitbox_paddingY*2;
       let hitbox_left = sprite.x + sprite.hitbox_paddingX;
-      let hitbox_right = sprite.x + sprite.image_object.width - sprite.hitbox_paddingX;
+      let hitbox_right = hitbox_left + hitbox_dx;
       let hitbox_top = sprite.y + sprite.hitbox_paddingY;
-      let hitbox_bottom = sprite.y + sprite.image_object.height - sprite.hitbox_paddingY;
+      let hitbox_bottom = hitbox_top + hitbox_dy;
+      let centerX = hitbox_left + hitbox_dx/2;
+      let centerY = hitbox_top + hitbox_dy/2;
+
+      //ladder
+      if(sprite.is_climbing){
+        //console.log("tick ladder");
+        for(let l = 0; l < this.b_m.ladder.length; l++){
+          let ladder = this.b_m.ladder[l];
+          if(centerX > ladder.x && centerX < ladder.x + ladder.dx && centerY > ladder.y && centerY < ladder.y + ladder.dy){
+            sprite.y -= sprite.walk_speed*1.5;
+          }
+        }
+      }
+
 
       //rooms
       for(let c = 0; c < this.b_m.chamber.length; c++){
         let chamber = this.b_m.chamber[c];
-        let x_dst = chamber.x - hitbox_left;//positive if out
-        let y_dst = chamber.y - hitbox_top;//positive if out
-        let dx_dst = chamber.x + chamber.dx - hitbox_right;//negative if out
-        let dy_dst = chamber.y + chamber.dy - hitbox_bottom;//negative if out
-        //console.log(`${x_dst} ${dx_dst} ${y_dst} ${dy_dst}`);
 
-        if(x_dst > 0){
-          sprite.x += x_dst;
-        }
-        if(dx_dst < 0){
-          sprite.x += dx_dst;
-        }
-        if(y_dst > 0){
-          sprite.y += y_dst;
-        }
-        if(dy_dst < 0){
-          sprite.y += dy_dst;
-        }
+        //if is indoor, check if is in the chambers, if outdoor, check if hit the chambers
+        if(sprite.is_indoor){
+          let result = this.checkDist(hitbox_left, hitbox_top, hitbox_right, hitbox_bottom, chamber.x, chamber.y, chamber.x + chamber.dx, chamber.y + chamber.dy);
+          //console.log(`${x_dst} ${dx_dst} ${y_dst} ${dy_dst}`);
 
-      }  
+          if(result[0] > 0){
+            sprite.x += result[0];
+          }
+          if(result[2] < 0){
+            sprite.x += result[2];
+          }
+          if(result[1] > 0){
+            sprite.y += result[1];
+          }
+          if(result[3] < 0){
+            sprite.y += result[3];
+          }
+        }else{
+          //let result = this.checkDist(chamber.x, chamber.y, chamber.x + chamber.dx, chamber.y + chamber.dy);
+        }
+      }
+
+      //boxes(including functional blocks)
+      sprite.beside_functional_block = false;
+      if(s == playerID){
+        for(let b = 0; b < this.b_m.box.length; b++){
+          let box = this.b_m.box[b];
+          let result = this.checkDist(hitbox_right, hitbox_bottom, hitbox_left, hitbox_top, box.x, box.y, box.x + box.dx, box.y + box.dy);//(sprite:box)b:t:neg, t:b:pos, l:r:pos, r:l:neg
+          
+          if(result[0] < 0 && result[1] < 0 && result[2] > 0 && result[3] > 0){//x_is_touched != -1 && y_is_touched != -1
+            if(box.openings[0]){
+              sprite.beside_functional_block = box;
+              //console.log("is touching the block");
+            }else{
+              //do it later
+            }
+          }
+        }
+      }
+    }
+
+    //ship colliding with terrain
+    if(true){
+      for(let t = 0; t < this.b_m.terrain.length; t++){
+        let terrain = this.b_m.terrain[t];
+        let terrain_offestX = terrain.x - Math.floor(ship_propertie.shipX);//offsets to the screen's origon
+        let terrain_offestY = terrain.y - Math.floor(ship_propertie.shipY);
+        let terrain_right = terrain_offestX + terrain.dx;
+        let terrain_bottom = terrain_offestY + terrain.dy;
+
+        for(let c = 0; c < this.b_m.chamber.length; c++){
+          let chamber = this.b_m.chamber[c];
+          let result = this.checkDist(chamber.x + chamber.dx, chamber.y + chamber.dy, chamber.x, chamber.y, terrain_offestX, terrain_offestY, terrain_right, terrain_bottom);
+          if(result[0] < 0 && result[1] < 0 && result[2] > 0 && result[3] > 0){
+            console.log("colliding with terrain");
+            ship_propertie.shipHit = true;
+            ship_propertie.health -= 1 * Math.ceil(ship_propertie.VelocityX + ship_propertie.VelocityY) * 10;
+            ship_propertie.VelocityX = -1 * ship_propertie.VelocityX * 1.5;//if hit a terrain, then bounce the ship and make it go the opposite direction with slower speed
+            ship_propertie.VelocityY = -1 * ship_propertie.VelocityY * 1.5;
+          }
+        } 
+      }       
     }
   }
 }
--- interactive_scene/sketch.js
+++ interactive_scene/sketch.js
@@ -1,4 +1,21 @@
-let display;
+let display = {
+  DEFAULT_CANVA : 400,
+  display_multiplier : 1,
+  zoom : 1,
+  mult : 1//this is what everything should multiply by
+};
+
+let ship_propertie = {
+  health : 100,
+  depth : 255,
+  shipX : 0,
+  VelocityX: 0,
+  VelocityY: 0,
+  shipHit : false,
+  shipY : 0,
+  goalX : 0,
+  goalY : 0,
+}
 
 let playerID;//player's sprite in the sprite list
 let sprite_manager;
@@ -8,48 +25,101 @@ let collision;
 
 function setup() {
   //display setup
-  display = {
-    DEFAULT_CANVA : 400,
-    display_multiplier : 1,
-    zoom : 1,
-    mult : 1//this is what everything should multiply by
-  };
-  createCanvas(windowWidth, windowHeight);
-  display.display_multiplier = (windowWidth < windowHeight? windowWidth : windowHeight) / 400;
+  let square = windowWidth < windowHeight? windowWidth : windowHeight;
+  createCanvas(square, square);
+  display.display_multiplier = square / 400;
   display.mult = display.display_multiplier * display.zoom;
 
 
   sprite_manager = new SpriteManager();
   playerID = sprite_manager.createSprite(20, 20, "assets/sprite.png");
   barrier_manager = new BarrierManager();
-  barrier_manager.addChamber(0, 0, 200, 200);
   collision = new Collision(sprite_manager, barrier_manager);
+
+  //submarine
+  //choose a random coordinate as goal
+  ship_propertie.goalX = Math.floor(Math.random() * 5000);
+  ship_propertie.goalX = Math.floor(Math.random() * 5000);
+
+  barrier_manager.addChamber(0, 0, 200, 200);
+  barrier_manager.addLadder(180, 180);
+  let driver_seatID = barrier_manager.addBox(180, 170, 10,10);
+  let driver_seat = barrier_manager.box[driver_seatID]
+  driver_seat.openings[0] = true;
+  driver_seat.openings[1] = new DriverSeat();
+  console.log(ship_propertie.ship_health);
+
+  //terrain
+  barrier_manager.addTerrain(200, 200, 50, 50);
 }
 
 
 function draw() {
-  background(220);
+  background(0, 0, ship_propertie.depth);
+
+  
   let player = sprite_manager.sprite_list[playerID];
   noSmooth();
   image(player.image_object, player.x * display.mult, player.y * display.mult, player.image_object.width * display.mult, player.image_object.height * display.mult);
 
   //render chambers
-  for(let i = 0; i < barrier_manager.current_chamberID; i++){
+  for(let i = 0; i < barrier_manager.chamber.length; i++){
     let chamber = barrier_manager.chamber[i];
     fill(0, 0, 0, 0)
     rect(chamber.x * display.mult, chamber.y * display.mult, chamber.dx * display.mult, chamber.dy * display.mult);
   };
+  //render ladders
+  for(let i = 0; i < barrier_manager.ladder.length; i++){
+    let ladder = barrier_manager.ladder[i];
+    image(ladder.image_object, ladder.x * display.mult, ladder.y * display.mult, ladder.dx * display.mult, ladder.dy * display.mult);
+  };
+
+  //box and functional stations
+  for(let i = 0; i < barrier_manager.box.length; i++){
+    let box = barrier_manager.box[i];
+    fill(150);
+    rect(box.x * display.mult, box.y * display.mult, box.dx * display.mult, box.dy * display.mult);
+  }
+
+  //render terrain
+  for(let i = 0; i < barrier_manager.terrain.length; i++){
+    let terrain = barrier_manager.terrain[i];
+    fill(0);
+    let terrain_offestX = terrain.x - Math.floor(ship_propertie.shipX);//offsets to the screen's origon
+    let terrain_offestY = terrain.y - Math.floor(ship_propertie.shipY); 
+    rect(terrain_offestX * display.mult, terrain_offestY * display.mult, terrain.dx * display.mult, terrain.dy * display.mult);
+  }
 
   playerInput();
   tick_phisics();
+
+  //tick the functional block logic
+  if(player.using_block){
+    player.beside_functional_block.openings[1].working(display.mult, ship_propertie);
+  }
+
+  smooth();
+  fill(0, 0, 0);
+  textSize(10 * display.mult);
+  text(`Ship Health: ${ship_propertie.health} \n X: ${ship_propertie.shipX} Y: ${ship_propertie.shipY}`, (display.DEFAULT_CANVA - 80) * display.mult, 10 * display.mult);
 }
 
 
 //**********************************************************************input
 function playerInput(){
   //console.log("pressed")
-  const input_list = [87, 65, 83, 68];
   let player = sprite_manager.sprite_list[playerID];
+
+  if(keyIsDown(82)){
+    player.using_block = false;
+  }
+
+  if(player.using_block){
+    return;
+  }
+
+  const input_list = [87, 65, 83, 68, 69];
+  player.is_climbing = false;
   
   for(let i = 0; i < input_list.length; i++){
     if(!keyIsDown(input_list[i])){
@@ -57,12 +127,20 @@ function playerInput(){
     }
     
     switch (i){
+    case 0:
+      player.is_climbing = true;
+      break;
     case 1:
       player.x -= player.walk_speed;
       break;
     case 3:
       player.x += player.walk_speed;
       break;
+    case 4:
+      console.log("press E");
+      if(typeof player.beside_functional_block === 'object'){
+        player.using_block = player.beside_functional_block.openings[0];
+      }
     }
   }
 }
@@ -72,7 +150,7 @@ function tick_phisics(){
   let player = sprite_manager.sprite_list[playerID];
   player.y += 1;
 
-  collision.tickCollision();
+  collision.tickCollision(playerID, ship_propertie, display);
 }
 
 
